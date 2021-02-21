8f486844d46cf445f391297a7c77a125a69bd883
Melchiah <noe_fam_chavez@yahoo.com.mx>
2021-02-21 10:54:20 -0600

    Utilizando View Binding en activity's.

--- app/src/main/java/mx/com/disoftware/viewbinding/MainActivity.kt
+++ app/src/main/java/mx/com/disoftware/viewbinding/MainActivity.kt
@@ -2,10 +2,23 @@ package mx.com.disoftware.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.com.disoftware.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Inflar el activity (la vista) activity_main, para ello utilizamos layoutInflater (nos regresa la vista).
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        // Obtener acceso a los recursos del activity_main.xml.
+        val view = activityMainBinding.root
+        // Vincular el xml con este archvio kotlin.
+        setContentView(view)
+
+        activityMainBinding.textViewMsg.text = "Utilizando View Binding"
+        activityMainBinding.textViewMsg.textSize = 25f;
+
     }
 }
