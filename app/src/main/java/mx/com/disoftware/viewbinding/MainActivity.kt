796b5736ab6d47107fac3b9c6c6e27f3c2300191
Melchiah <noe_fam_chavez@yahoo.com.mx>
2021-02-21 13:02:46 -0600

    funcionamiento incorrecto.

--- app/src/main/java/mx/com/disoftware/viewbinding/MainActivity.kt
+++ app/src/main/java/mx/com/disoftware/viewbinding/MainActivity.kt
@@ -3,6 +3,7 @@ package mx.com.disoftware.viewbinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import mx.com.disoftware.viewbinding.databinding.ActivityMainBinding
+import mx.com.disoftware.viewbinding.databinding.FragmentViewBindingBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +19,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         activityMainBinding.textViewMsg.text = "Utilizando View Binding"
+
         activityMainBinding.textViewMsg.textSize = 25f;
+
+        /** Ver cómusniona un fragmento y corregir app, la idea es que noa paresca el fragmento
+         * hasta que se de click en el botón, y que éstos (botón como el textview) se quiten y el
+         * fragmento abarque todo el activity.
+         * https://developer.android.com/guide/components/fragments?hl=es
+         */
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        activityMainBinding.buttonActivityMain.setOnClickListener {
+            val fragment = ViewBindingFragment()
+            fragmentTransaction.add(R.id.fragment_container, fragment)
+            fragmentTransaction.commit()
+        }
 
     }
 }
+
+// https://developer.android.com/topic/libraries/view-binding#kotlin
---
+++ app/src/main/java/mx/com/disoftware/viewbinding/ViewBindingFragment.kt
@@ -0,0 +1,48 @@
+package mx.com.disoftware.viewbinding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mx.com.disoftware.viewbinding.databinding.FragmentViewBindingBinding
+
+class ViewBindingFragment : Fragment() {
+
+    private var _viewBindingFragment: FragmentViewBindingBinding? = null
+
+    // Esta propiedad es válida sólo cuando se ejecuta el onCreateView hasta el onDestroyView.
+    private val viewBindingFragment get() = _viewBindingFragment!!
+
+    // Por el onCreate se infla el fragment.
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // inflar la vista del fragmento.
+        _viewBindingFragment = FragmentViewBindingBinding.inflate(inflater, container, false)
+        // accceder a los recuros del fragmento.
+        val view = viewBindingFragment.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var acum = 0
+        viewBindingFragment.textViewMsgFragment.text = "View Binding en Fragmentos"
+        viewBindingFragment.buttonFragment.setOnClickListener {
+            viewBindingFragment.textViewMsgFragment.text = "me has presionado ${acum++}"
+            viewBindingFragment.textViewMsgFragment.textSize = 25f
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // liverar la memoria cuando se destruya este fragmento.
+        _viewBindingFragment = null
+    }
+
+}
+
+// https://www.youtube.com/watch?v=31XwN-Ztba4&t=293s (para el reporte ver de nuevo)
